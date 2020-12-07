3d2bfbb5d1fc302fed1fa5b52f51a699a79ebbb6
Kenny Deriemaeker <kenny.deriemaeker@inthepocket.com>
2020-12-07 21:13:26 +0100

    Add .gitignore and main.cpp.

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "🐙 The most merciful thing in the world, I think, is the inability of the "
+    "human mind to correlate all its contents. We live on a placid island of ignorance "
+    "in the midst of black seas of infinity, and it was not meant that we should voyage far. 🏝" << std::endl;
+    
+  return 0;
+}
